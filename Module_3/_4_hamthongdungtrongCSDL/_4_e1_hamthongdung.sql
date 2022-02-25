ebf6dcf601dcf90fcaf6af66fd425ed801ea4086
QuangHuynh96 <lqh.nuchaku@gmail.com>
2022-02-25 22:32:13 +0700

    fix_exrcise

--- Module_3/_4_hamthongdungtrongCSDL/_4_e1_hamthongdung.sql
+++ Module_3/_4_hamthongdungtrongCSDL/_4_e1_hamthongdung.sql
@@ -1,12 +1,16 @@
 use studentmanagement;
 
 -- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
-select subName, max(credit) 
-from `subject`;
+select *
+from `subject`
+where credit = (select max(credit) from `subject`);
 
 -- Hiển thị các thông tin môn học có điểm thi lớn nhất.
-select *, max(mark)
-from `subject` join mark on `subject`.subid = mark.subid;
+select *
+from `subject` join mark on `subject`.subid = mark.subid
+where mark = (
+select max(mark)
+from mark);
 
 -- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
 select *, avg(mark) as 'Điểm TB các môn học'
